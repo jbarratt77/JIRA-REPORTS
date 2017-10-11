0c4f8cb9de7b41c7b245981da9ce4d2a72e45752
ioanaoprescu <>
2017-10-11 18:26:20 +0100

    :sparkles: get epic data

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,8 @@
-package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication (scanBasePackages = {"configuration", "controller", "model", "service" })
 public class Application {
 
     public static void main(String[] args) {
